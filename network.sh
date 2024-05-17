eaffb7b0c8b90d8b426622f6337eb7a1e5318ee8
Jidokexe <154472201+Jidokexe@users.noreply.github.com>
2024-05-17 15:42:40 +0300

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
 # Логирование действий
-exec &> /var/log/change_ip_address.log
+LOG_FILE="/var/log/change_ip_address.log"
+exec > >(tee -a ${LOG_FILE} )
+exec 2>&1
 
-# Проверка наличия прав доступа
-if [ "$EUID" -ne 0 ]; then
-  echo "Этот скрипт должен быть запущен с правами суперпользователя"
-  exit 1
+# Проверка наличия необходимых прав доступа
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Ошибка: Необходимы права суперпользователя для выполнения скрипта." >&2
+    exit 1
 fi
 
 # Проверка передачи аргументов
 if [ "$#" -ne 2 ]; then
-  echo "Использование: $0 <название интерфейса> <новый IP-адрес>"
-  exit 1
+    echo "Использование: $0 <interface> <new_ip_address>"
+    exit 1
 fi
 
 interface=$1
 new_ip=$2
 
 # Проверка существования сетевого интерфейса
-if ! ip link show $interface &> /dev/null; then
-  echo "Сетевой интерфейс $interface не найден"
-  exit 1
+if ! ip a show dev $interface > /dev/null 2>&1; then
+    echo "Ошибка: Сетевой интерфейс $interface не найден."
+    exit 1
 fi
 
 # Изменение IP-адреса путем редактирования файла сетевой конфигурации
-sed -i "/^address/s/.*/address $new_ip/" /etc/network/interfaces
+sed -i "/^address/s/.*/address $new_ip/" /etc/sysconfig/network-scripts/ifcfg-enp1s0
 
 if [ $? -eq 0 ]; then
-  echo "IP-адрес интерфейса $interface успешно изменен на $new_ip"
+    echo "Успешно изменили IP-адрес на $new_ip для интерфейса $interface."
+    # Вывод информации о текущей сетевой конфигурации
+    ip a show dev $interface
 else
-  echo "Не удалось изменить IP-адрес интерфейса $interface"
+    echo "Ошибка: не удалось изменить IP-адрес для интерфейса $interface."
+    exit 1
 fi
 
 # Перезагрузка сетевого интерфейса
